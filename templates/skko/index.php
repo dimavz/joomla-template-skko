13a6132c93c3c918e58d36435a68594bdf509461
Dmitry <zatulenko@gmail.com>
2017-11-29 17:43:58 +0300

    Оставлена анимация шапки сайта только на главной странице

--- templates/skko/index.php
+++ templates/skko/index.php
@@ -155,7 +155,7 @@ if ($doc->countModules('position-18'))
 							</div>
 						<?php endif;?>
 						<?php if($showMobileMenu):?>
-								<jdoc:include type="modules" name="position-18"/>
+							<jdoc:include type="modules" name="position-18"/>
 						<?php endif;?>
 					</div><!-- /.top_main_menu -->
 					<div class="col-md-6 col-sm-6 col-xs-3">
@@ -164,8 +164,15 @@ if ($doc->countModules('position-18'))
 								<span class="glyphicon glyphicon-align-justify" aria-hidden="true"></span>
 							</button>
 							<a href="/">
-								<h1 class="hidden-xs wow fadeInDown"><?php echo $site_name ?  $site_name : 'SKKO.BY'?></h1>
+								<!-- Выводим анимацию названия сайта только на главной странице-->
+								<?php if($activePage == $defaultPage) :?>
+									<h1 class="hidden-xs wow fadeInDown"><?php echo $site_name ?  $site_name : 'SKKO.BY'?></h1>
+								<?php else:?>
+									<h1 class="hidden-xs"><?php echo $site_name ?  $site_name : 'SKKO.BY'?></h1>
+								<?php endif;?>
 							</a>
+
+							<!-- Конец вывода анимации названия сайта на главной странице-->
 						</div>
 						<div class="icon_search visible-sm-block"><i class="fa fa-search fa-2x" aria-hidden="true"></i>
 
@@ -188,204 +195,211 @@ if ($doc->countModules('position-18'))
 			<div class="row">
 				<div class="wraper_logo">
 					<div class="col-md-3 col-sm-3 col-xs-4">
-						<div class="logo_img wow bounceInDown" data-wow-delay="0.5s">
-							<?php if($site_logo) :?>
-								<a href="<?php echo JUri::base();?>">
-									<img src="<?php echo $site_logo ?>" alt="<?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования' ?>">
-								</a>
+						<?php if($activePage == $defaultPage) :?> <!-- Если главная страница -->
+							<div class="logo_img wow bounceInDown" data-wow-delay="0.5s">
 							<?php else:?>
-								<a href="<?php echo JUri::base();?>">
-									<img src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/images/logo.png" alt="<?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования' ?>">
-								</a>
+								<div class="logo_img">
+								<?php endif; ?>
+								<?php if($site_logo) :?>
+									<a href="<?php echo JUri::base();?>">
+										<img src="<?php echo $site_logo ?>" alt="<?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования' ?>">
+									</a>
+								<?php else:?>
+									<a href="<?php echo JUri::base();?>">
+										<img src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/images/logo.png" alt="<?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования' ?>">
+									</a>
+								<?php endif;?>
+							</div>
+						</div>
+						<div class="col-md-9 col-sm-9 col-xs-8">
+							<div class="logo_text">
+								<div class="logo_title visible-xs-block">
+									<a href="/"><h1><?php echo $site_name ?  $site_name : 'SKKO.BY'?></h1></a>
+								</div>
+								<?php if($activePage == $defaultPage) :?> <!-- Если главная страница -->
+									<div class="greeting  wow flipInX">
+									<?php else:?><!-- Если НЕ главная страница -->
+										<div class="greeting">
+										<?php endif;?> <!-- Конец блока -->
+										<div class="row">
+											<div class="col-md-9 col-sm-9">
+												<h3><?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования.' ?></h3> 
+												<span>Управляйте своим бизнесом через Internet</span>
+											</div>
+											<?php if ($showButtons):?>
+												<!-- Кнопки Войти с ЭЦП и Почта -->
+												<div class="col-md-3 col-sm-3">
+													<jdoc:include type="modules" name="position-2"/>
+												</div>
+											<?php endif;?>
+										</div><!-- /.row -->
+										<?php if($showMessage):?>
+											<div class="row hidden-xs">
+												<!-- Блок Внимание!!! -->
+												<jdoc:include type="modules" name="position-3" style="message"/>
+											</div>
+										<?php endif;?>
+									</div>
+								</div>
+							</div>
+						</div><!-- /.wraper_logo -->
+					</div>
+
+					<?php if($showMenu):?>
+						<!-- Модуль Меню -->
+						<jdoc:include type="modules" name="position-4" style="default"/>
+					<?php endif;?>
+					<?php if($showMessage):?>
+						<div class="row visible-xs">
+							<!-- Блок Внимание!!! -->
+							<jdoc:include type="modules" name="position-3" style="message"/>
+						</div>
+					<?php endif;?>
+				</div>
+			</header>
+			<section class="content">
+				<div class="container">
+					<?php if($showBanners):?>
+						<div class="row">
+							<!-- Блок Банеров!!! -->
+							<jdoc:include type="modules" name="position-5" style="default"/>
+						</div>
+					<?php endif;?>
+					<?php if($activePage != $defaultPage) :?>
+						<div class="row main_content">
+							<?php if($showBreadcrumbs):?>
+								<div class="col-md-12">
+									<!-- Модуль Путь ссылок -->
+									<nav class="path_links">
+										<jdoc:include type="modules" name="position-7"/>
+									</nav>
+								</div>
 							<?php endif;?>
 
-						</div>
-					</div>
-					<div class="col-md-9 col-sm-9 col-xs-8">
-						<div class="logo_text">
-							<div class="logo_title visible-xs-block">
-								<a href="/"><h1><?php echo $site_name ?  $site_name : 'SKKO.BY'?></h1></a>
-							</div>
-							<div class="greeting  wow flipInX">
-								<div class="row">
-									<div class="col-md-9 col-sm-9">
-										<h3><?php echo $site_desc ?  $site_desc : 'СККО - Система контроля кассового оборудования.' ?></h3> 
-										<span>Управляйте своим бизнесом через Internet</span>
-									</div>
-									<?php if ($showButtons):?>
-										<!-- Кнопки Войти с ЭЦП и Почта -->
-										<div class="col-md-3 col-sm-3">
-											<jdoc:include type="modules" name="position-2"/>
-										</div>
-									<?php endif;?>
-								</div><!-- /.row -->
-								<?php if($showMessage):?>
-									<div class="row hidden-xs">
-										<!-- Блок Внимание!!! -->
-										<jdoc:include type="modules" name="position-3" style="message"/>
-									</div>
+							<?php if($showSidebar):?>
+								<!-- Контент -->
+								<div class="col-md-9">
+									<jdoc:include type="message" />
+									<jdoc:include type="component" />
+								</div>
+								<!-- Сайдбар -->
+								<div class="col-md-3">
+									<!-- Модули Сайдбара -->
+									<div class="block_sidebar">
+										<jdoc:include type="modules" name="position-8"/>
+									</div>	
+								</div>
+								<div class="clearfix"></div>
+							<?php else:?>
+								<!-- Контент -->
+								<div class="col-md-12">
+									<jdoc:include type="message" />
+									<jdoc:include type="component" />
+								</div>
+								<div class="clearfix"></div>
+							<?php endif;?>
+
+							<!-- Путь ссылок -->
+							<?php if($showBreadcrumbs):?>
+								<div class="col-md-12">
+									<!-- Модуль Путь ссылок -->
+									<nav class="path_links">
+										<jdoc:include type="modules" name="position-7"/>
+									</nav>
+								</div>
+							<?php endif;?>
+						</div><!-- /.row main_content -->
+					<?php else :?>
+						<div class="row">
+							<div class="modules">
+								<?php if($showNewsModule): ?>
+									<jdoc:include type="modules" name="position-6" style="news"/>
+								<?php endif; ?>
+							</div><!-- /.modules -->
+						</div><!-- /.row -->
+					<?php endif;?>
+					<?php if($showRowModules): ?>
+						<div class="row part_links">
+							<?php if($showLeftModule): ?>
+								<jdoc:include type="modules" name="position-10" style="left_block"/>
+							<?php endif;?>
+							<?php if($showCenterModule): ?>
+								<jdoc:include type="modules" name="position-11" style="center_block"/>
+							<?php endif;?>
+							<?php if($showRightModule): ?>
+								<jdoc:include type="modules" name="position-12" style="right_block"/>
+							<?php endif;?>
+						</div><!-- /.row part_links-->
+					<?php endif;?>
+				</div><!-- /.container -->
+			</section>
+			<footer class="wow pulse">
+				<div class="main_footer">
+					<div class="footer_subscribe">
+						<div class="container">
+							<div class="row">
+								<?php if($showSubscribeModule): ?>
+									<jdoc:include type="modules" name="position-9"/>
 								<?php endif;?>
 							</div>
 						</div>
 					</div>
-				</div><!-- /.wraper_logo -->
+					<?php if($showFooterModules): ?>
+						<div class="footer_topline">
+							<div class="container">
+								<div class="row">
+									<?php if($showFooterLeftModules): ?>
+										<jdoc:include type="modules" name="position-13" style="footer_left_block"/>
+									<?php endif;?>
+									<?php if($showFooterCenterModules): ?>
+										<jdoc:include type="modules" name="position-14" style="footer_center_block"/>
+									<?php endif;?>
+									<?php if($showFooterRightModules): ?>
+										<jdoc:include type="modules" name="position-15" style="footer_right_block"/>
+									<?php endif;?>
+								</div><!-- /.row -->
+							</div><!-- /.container -->
+						</div><!-- /.footer_topline -->
+					<?php endif;?>
+					<?php if($showFooterBottomLine): ?>
+						<div class="footer_bottomline">
+							<div class="container">
+								<div class="row">
+									<jdoc:include type="modules" name="position-16"/>
+								</div>
+							</div>
+						</div>
+					<?php endif;?>
+					<div class="clearfix"></div>
+				</div>
+			</footer>
+			<div class="hidden">
 			</div>
-
-			<?php if($showMenu):?>
-				<!-- Модуль Меню -->
-				<jdoc:include type="modules" name="position-4" style="default"/>
-			<?php endif;?>
-			<?php if($showMessage):?>
-				<div class="row visible-xs">
-					<!-- Блок Внимание!!! -->
-					<jdoc:include type="modules" name="position-3" style="message"/>
-				</div>
-			<?php endif;?>
-		</div>
-	</header>
-	<section class="content">
-		<div class="container">
-			<?php if($showBanners):?>
-				<div class="row">
-					<!-- Блок Банеров!!! -->
-					<jdoc:include type="modules" name="position-5" style="default"/>
-				</div>
-			<?php endif;?>
-			<?php if($activePage != $defaultPage) :?>
-				<div class="row main_content">
-					<?php if($showBreadcrumbs):?>
-						<div class="col-md-12">
-							<!-- Модуль Путь ссылок -->
-							<nav class="path_links">
-								<jdoc:include type="modules" name="position-7"/>
-							</nav>
-						</div>
-					<?php endif;?>
-
-					<?php if($showSidebar):?>
-						<!-- Контент -->
-						<div class="col-md-9">
-							<jdoc:include type="message" />
-							<jdoc:include type="component" />
-						</div>
-						<!-- Сайдбар -->
-						<div class="col-md-3">
-							<!-- Модули Сайдбара -->
-							<div class="block_sidebar">
-								<jdoc:include type="modules" name="position-8"/>
-							</div>	
-						</div>
-						<div class="clearfix"></div>
-					<?php else:?>
-						<!-- Контент -->
-						<div class="col-md-12">
-							<jdoc:include type="message" />
-							<jdoc:include type="component" />
-						</div>
-						<div class="clearfix"></div>
-					<?php endif;?>
-
-					<!-- Путь ссылок -->
-					<?php if($showBreadcrumbs):?>
-						<div class="col-md-12">
-							<!-- Модуль Путь ссылок -->
-							<nav class="path_links">
-								<jdoc:include type="modules" name="position-7"/>
-							</nav>
-						</div>
-					<?php endif;?>
-				</div><!-- /.row main_content -->
-			<?php else :?>
-				<div class="row">
-					<div class="modules">
-						<?php if($showNewsModule): ?>
-							<jdoc:include type="modules" name="position-6" style="news"/>
-						<?php endif; ?>
-					</div><!-- /.modules -->
-				</div><!-- /.row -->
-			<?php endif;?>
-			<?php if($showRowModules): ?>
-				<div class="row part_links">
-					<?php if($showLeftModule): ?>
-						<jdoc:include type="modules" name="position-10" style="left_block"/>
-					<?php endif;?>
-					<?php if($showCenterModule): ?>
-						<jdoc:include type="modules" name="position-11" style="center_block"/>
-					<?php endif;?>
-					<?php if($showRightModule): ?>
-						<jdoc:include type="modules" name="position-12" style="right_block"/>
-					<?php endif;?>
-				</div><!-- /.row part_links-->
-			<?php endif;?>
-		</div><!-- /.container -->
-	</section>
-	<footer class="wow pulse">
-		<div class="main_footer">
-			<div class="footer_subscribe">
-				<div class="container">
-					<div class="row">
-						<?php if($showSubscribeModule): ?>
-							<jdoc:include type="modules" name="position-9"/>
-						<?php endif;?>
-					</div>
-				</div>
-			</div>
-			<?php if($showFooterModules): ?>
-				<div class="footer_topline">
-					<div class="container">
-						<div class="row">
-							<?php if($showFooterLeftModules): ?>
-								<jdoc:include type="modules" name="position-13" style="footer_left_block"/>
-							<?php endif;?>
-							<?php if($showFooterCenterModules): ?>
-								<jdoc:include type="modules" name="position-14" style="footer_center_block"/>
-							<?php endif;?>
-							<?php if($showFooterRightModules): ?>
-								<jdoc:include type="modules" name="position-15" style="footer_right_block"/>
-							<?php endif;?>
-						</div><!-- /.row -->
-					</div><!-- /.container -->
-				</div><!-- /.footer_topline -->
-			<?php endif;?>
-			<?php if($showFooterBottomLine): ?>
-				<div class="footer_bottomline">
-					<div class="container">
-						<div class="row">
-							<jdoc:include type="modules" name="position-16"/>
-						</div>
-					</div>
-				</div>
-			<?php endif;?>
-			<div class="clearfix"></div>
-		</div>
-	</footer>
-	<div class="hidden">
-	</div>
 	<!--[if lt IE 9]>
 	<script src="libs/html5shiv/es5-shim.min.js"></script>
 	<script src="libs/html5shiv/html5shiv.min.js"></script>
 	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
 	<script src="libs/respond/respond.min.js"></script>
-	<![endif]-->
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
+<![endif]-->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
 
-	<!-- Подключение скриптов (Scripts) -->
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/jquery/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/waypoints/waypoints-1.6.2.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/scrollto/jquery.scrollTo.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/owl-carousel2/owl.carousel.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.plugin.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.countdown.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.countdown-ru.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/landing-nav/navigation.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/wow.min.js"></script>
-	<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/common.js"></script>
-	
-	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
-	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+<!-- Подключение скриптов (Scripts) -->
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/jquery/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/fancybox/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/waypoints/waypoints-1.6.2.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/scrollto/jquery.scrollTo.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/owl-carousel2/owl.carousel.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.plugin.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/countdown/jquery.countdown-ru.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/libs/landing-nav/navigation.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/wow.min.js"></script>
+<script type="text/javascript" src="<?php echo JUri::base();?>templates/<?php echo $doc->template; ?>/js/common.js"></script>
+
+<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+<!-- Google Analytics counter --><!-- /Google Analytics counter -->
 </body>
 </html>
