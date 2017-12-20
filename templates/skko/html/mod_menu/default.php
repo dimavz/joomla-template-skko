4513558dba639e873ef4a4ad4d4f309299c908bd
Dmitry <zatulenko@gmail.com>
2017-12-20 11:22:13 +0300

    Сделаны изменения в шаблоне меню

--- templates/skko/html/mod_menu/default.php
+++ templates/skko/html/mod_menu/default.php
@@ -18,8 +18,7 @@ if ($tagId = $params->get('tag_id', ''))
 
 // The menu class is deprecated. Use nav instead
 ?>
-<nav class="main_menu hidden-xs affix" data-spy="affix" data-offset-top="250">
-<ul>
+<ul class="hidden-xs">
 <?php foreach ($list as $i => &$item)
 {
 	//print_r($item);
@@ -28,44 +27,11 @@ if ($tagId = $params->get('tag_id', ''))
 		$class .= ' home';
 	}
 
-	// if ($item->id == $default_id)
-	// {
-		// $class .= ' active';
-	// }
-
-/* 	if ($item->id == $active_id || ($item->type === 'alias' && $item->params->get('aliasoptions') == $active_id))
-	{
-		$class .= ' active';
-	}
-	 */
 	// Если ID входит в путь ссылки, то делаем ссылку активной
 	if (in_array($item->id, $path))
 	{
 		$class .= ' active';
 	}
-
-	/* if (in_array($item->id, $path))
-	{
-		$class .= ' active';
-	}
-	elseif ($item->type === 'alias')
-	{
-		$aliasToId = $item->params->get('aliasoptions');
-
-		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
-		{
-			$class .= ' active';
-		}
-		elseif (in_array($aliasToId, $path))
-		{
-			$class .= ' active';
-		}
-	}  */
-
-	/* if ($item->type === 'separator')
-	{
-		$class .= ' divider';
-	} */
 
 	if ($item->deeper)
 	{
@@ -115,4 +81,4 @@ if ($tagId = $params->get('tag_id', ''))
 		echo '</li>';
 	}
 }
-?></ul></nav>
+?></ul>
