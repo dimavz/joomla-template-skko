dea4947801089a6bafe42b5602ed411ab60bf42d
Dmitry <zatulenko@gmail.com>
2017-12-20 12:39:19 +0300

    Убран код для отправки форм

--- templates/skko/js/common.js
+++ templates/skko/js/common.js
@@ -157,21 +157,7 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("form").submit(function() {
-		$.ajax({
-			type: "GET",
-			url: "mail.php",
-			data: $("form").serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-		return false;
-	});
+	
  // Фитксация главного меню
  var $menu = $("main_menu");
 
